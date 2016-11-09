899371f00c244c1bf83cf4bf20fe36033b5d81aa
Sofyan Thayf <sofyanthayf@gmail.com>
2016-11-09 08:26:45 +0800

    Penambahan file firebase.js
    Memisahkan script inisialisasi Firebase ke dalam file eksternal
    js/firebase.js
    
    ATTN!: jangan menulis/memanggil script apapun sebelum pemanggilan file
    firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyCIftc3ikXPurzpxsUYSvmR4laVR-ff3CQ",
+	authDomain: "bekup2-forum.firebaseapp.com",
+	databaseURL: "https://bekup2-forum.firebaseio.com",
+	storageBucket: "bekup2-forum.appspot.com",
+	messagingSenderId: "818273056014"
+};
+
+firebase.initializeApp(config);
